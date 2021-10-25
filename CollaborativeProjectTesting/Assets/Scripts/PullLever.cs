d9738e64ee0de47629989884e2120b3528936357
AntanasZ <D00218148@student.dkit.ie>
2021-10-25 13:30:19 +0100

    Added Comments
    - Added comments to explain methods

--- CollaborativeProjectTesting/Assets/Scripts/PullLever.cs
+++ CollaborativeProjectTesting/Assets/Scripts/PullLever.cs
@@ -11,40 +11,23 @@ public class PullLever : MonoBehaviour
     private CorrectLeverOrder correctOrder;
 
     [SerializeField]
-    private Timer timer;
+    private Timer timer; //used to call some timer functions
 
     private Ray ray;
     private int leverCount = 0;
     private string[] leverOrder = new string[5];
     public bool puzzleFinished = false;
-    
-
 
     private void Awake()
     {
         selectableTag = selectableTag.Trim();
     }
 
-    //private void Start()
-    //{
-        
-    //}
-
     private void Update()
     {
-        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        //RaycastHit hitInfo = new RaycastHit();
-        //if (Physics.Raycast(ray, out hitInfo))
-        //{
-        //    var currentSelection = hitInfo.transform;
-        //    if(currentSelection.CompareTag(selectableTag))
-        //    {
-
-        //    }
-        //}
         OnMouseOver();
 
+        //check if player finished puzzle
         if(leverCount == 5 && puzzleFinished == false)
         {
             puzzleFinished = true;
@@ -55,25 +38,31 @@ public class PullLever : MonoBehaviour
     // OnMouseOver is called every frame while the mouse is over the GUIElement or Collider
     private void OnMouseOver()
     {
+        //check if there are still levers to pull
         if(leverCount < 5)
         {
+            //clicking on lever
             if (Input.GetMouseButtonDown(0))
             {
                 ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hitInfo;
 
+                //get the lever that was clicked
                 if (Physics.Raycast(ray, out hitInfo))
                 {
+                    //add it to players lever order
                     leverOrder[leverCount] = hitInfo.transform.gameObject.name;
 
+                    //check if the lever pulled corresponds to the correct lever order
                     if (CheckIfCorrect())
                     {
+                        //if true, increment count and do something to the lever
                         leverCount++;
                         hitInfo.transform.Rotate(45, 0, 0);
                     }
                     else
                     {
-                        //decrease timer
+                        //if false, decrease timer
                         timer.decreaseTime();
                     }
                 }
@@ -83,6 +72,7 @@ public class PullLever : MonoBehaviour
 
     private bool CheckIfCorrect()
     {
+        //compare current lever that was pulled to the lever that was supposed to be pulled
         if (leverOrder[leverCount].Equals(correctOrder.LeverOrder[leverCount]))
         {
             return true;
--- CollaborativeProjectTesting/Assets/Scripts/Timer.cs
+++ CollaborativeProjectTesting/Assets/Scripts/Timer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//timer class was made with help from:
+//https://gamedevbeginner.com/how-to-make-countdown-timer-in-unity-minutes-seconds/
+
 public class Timer : MonoBehaviour
 {
     [SerializeField]
@@ -19,6 +22,7 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //start timer
         timerIsRunning = true;
     }
 
@@ -27,12 +31,13 @@ public class Timer : MonoBehaviour
     {
         if(timerIsRunning)
         {
+            //if timer is running and still time remaining, decrease and display remaining time
             if(timeRemaining > 0)
             {
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
             }
-            else
+            else //if timer runs out, stop timer and display appropriate text
             {
                 timeRemaining = 0;
                 timerIsRunning = false;
@@ -51,17 +56,20 @@ public class Timer : MonoBehaviour
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
+    //if player does the wrong action, timer gets decreased
     public void decreaseTime()
     {
         timeRemaining -= timeToDecrease;
     }
 
+    //if player solves puzzle, timer is stopped and appropriate text is displayed
     public void stopTimer()
     {
         timerIsRunning = false;
         displayFinishText("You Win");
     }
 
+    //changes timer text to something else
     private void displayFinishText(string finishText)
     {
         timeText.text = finishText;
